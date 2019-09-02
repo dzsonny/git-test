146740ad6f0a0080b7e5cebf9fd388d0084e6811
tom_glb <goodMorning_gld@adaf.com>
2019-09-02 12:25:42 +0800

    修改 这个First Class

--- src/com/itheima/git/FirstClass.java
+++ src/com/itheima/git/FirstClass.java
@@ -9,6 +9,8 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        //hello test
+
     }
 
     public void test() {
